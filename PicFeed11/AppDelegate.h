3c667c6573b1e5239a24a8b235a5f0b7f9b3aa44
daneozxy <daneozxy@yahoo-inc.com>
2015-01-22 19:31:53 -0800

    Initial Commit

---
+++ PicFeed11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicFeed11
+//
+//  Created by Xiangyu Zhang on 1/22/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PicFeed11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PicFeed11
+//
+//  Created by Xiangyu Zhang on 1/22/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
